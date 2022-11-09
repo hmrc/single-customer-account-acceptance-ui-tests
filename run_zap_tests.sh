87af7bc24ec1e3294d22afac732845dd15706044
Rakesh-Eluri <108217580+Rakesh-Eluri@users.noreply.github.com>
2022-11-09 17:05:35 +0000

    Changes in ZAP shell script

--- run_zap_tests.sh
+++ run_zap_tests.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
 ENV=${1:-local}
 BROWSER=${2:-chrome}
 
-if [ "$BROWSER" = "chrome" ]; then
-    DRIVER="-Dwebdriver.chrome.driver=/opt/homebrew/bin/chromedriver"
-elif [ "$BROWSER" = "firefox" ]; then
-    DRIVER="-Dwebdriver.gecko.driver=/opt/homebrew/bin/geckodriver"
-fi
+echo "*** running on $ENV using $BROWSER for tags '$tags' ***"
 
-sbt -Dhttp.proxyHost=localhost -Dhttp.proxyPort=11000 -Dbrowser=$BROWSER -Denvironment=$ENV $DRIVER "testOnly uk.gov.hmrc.test.ui.cucumber.runner.ZapRunner"
+sbt -Dhttp.proxyHost=localhost -Dhttp.proxyPort=11000 -Dbrowser=$BROWSER -Denvironment=$ENV -Dcucumber.options="--tags '$tags'" clean "testOnly uk.gov.hmrc.test.ui.cucumber.runner.ZapRunner"
