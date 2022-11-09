31a088d2677460330083f6c71340e09f26738600
Rakesh-Eluri <108217580+Rakesh-Eluri@users.noreply.github.com>
2022-11-09 20:33:08 +0000

    Changes in ZAP shell script

--- run_zap_tests.sh
+++ run_zap_tests.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-ENV=${1:-local}
-BROWSER=${2:-chrome}
+browser="chrome"
+if [ $# -gt 0  ];
+then
+  browser="$1"
+fi
 
-echo "*** running on $ENV using $BROWSER for tags '$tags' ***"
+environment="local"
 
-sbt -Dhttp.proxyHost=localhost -Dhttp.proxyPort=11000 -Dbrowser=$BROWSER -Denvironment=$ENV -Dcucumber.options="--tags '$tags'" clean "testOnly uk.gov.hmrc.test.ui.cucumber.runner.ZapRunner"
+echo "*** running on $environment using $browser for tags '$tags' ***"
+
+sbt -Dhttp.proxyHost=localhost -Dhttp.proxyPort=11000 -Denvironment="$environment" -Dbrowser="$browser" -Dcucumber.options="--tags '$tags'" clean "testOnly uk.gov.hmrc.test.ui.cucumber.runner.ZapRunner"
