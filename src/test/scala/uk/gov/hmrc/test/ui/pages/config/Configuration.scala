a601f7593c74cc4cb504bd2491f0dd92fbcd7f05
Yang Wong <20066247+yangwong9664@users.noreply.github.com>
2022-11-11 13:42:03 +0000

    refactor code

--- src/test/scala/uk/gov/hmrc/test/ui/pages/config/Configuration.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/config/Configuration.scala
@@ -23,82 +23,38 @@ case class Configuration(
   FEEDBACK_PAGE: String,
   STATEPENSION_PAGE: String,
   NI_PAGE: String,
-  PAGE_TIMEOUT_SECS: Int
+  PAGE_TIMEOUT_SECS: Int = 60
 )
 
 object Configuration {
 
-  lazy val environment: Environment.Name = {
-
-    val environmentProperty =
-      System.getProperty("environment", "local").toLowerCase
-    environmentProperty match {
-      case "local"   => Environment.Local
-      case "dev"     => Environment.dev
-      case "qa"      => Environment.Qa
-      case "staging" => Environment.Staging
-      case _         =>
-        throw new IllegalArgumentException(s"Environment '$environmentProperty' not known")
+  lazy val environment: Option[String] = {
+    System.getProperty("environment", "local").toLowerCase match {
+      case "local"   => None
+      case "dev"     => Some("https://www.development.tax.service.gov.uk")
+      case "qa"      => Some("https://www.qa.tax.service.gov.uk")
+      case "staging" => Some("https://www.staging.tax.service.gov.uk")
+      case environmentProperty => throw new IllegalArgumentException(s"Environment '$environmentProperty' not known")
     }
   }
 
-  lazy val settings: Configuration = create()
-
-  private def create(): Configuration =
-    environment match {
-
-      case Environment.Local =>
-        new Configuration(
-          APP_ROOT = "http://localhost:9949/auth-login-stub/gg-sign-in?continue=%2Fsingle-customer-account",
-          APPROOT = "http://localhost:8420/single-customer-account",
-          AUTHLOGINSTUB = "http://localhost:9949/auth-login-stub/gg-sign-in",
-          FEEDBACK_PAGE = "http://localhost:9514/feedback/single-customer-account-frontend",
-          STATEPENSION_PAGE = "http://localhost:9234/check-your-state-pension/account",
-          NI_PAGE = "http://localhost:9234/check-your-state-pension/account/nirecord",
-          PAGE_TIMEOUT_SECS = 60
-        )
-
-      case Environment.dev =>
-        new Configuration(
-          APP_ROOT =
-            "https://www.development.tax.service.gov.uk/auth-login-stub/gg-sign-in?continue=%2Fsingle-customer-account",
-          APPROOT = "https://www.development.tax.service.gov.uk/single-customer-account",
-          AUTHLOGINSTUB = "https://www.development.tax.service.gov.uk/auth-login-stub/gg-sign-in",
-          FEEDBACK_PAGE = "https://www.development.tax.service.gov.uk/feedback/single-customer-account-frontend",
-          STATEPENSION_PAGE = "https://www.development.tax.service.gov.uk/check-your-state-pension/account",
-          NI_PAGE = "https://www.development.tax.service.gov.uk/check-your-state-pension/account/nirecord",
-          PAGE_TIMEOUT_SECS = 60
-        )
-
-      case Environment.Qa =>
-        new Configuration(
-          APP_ROOT = "https://www.qa.tax.service.gov.uk/auth-login-stub/gg-sign-in?continue=%2Fsingle-customer-account",
-          APPROOT = "https://www.qa.tax.service.gov.uk/single-customer-account",
-          AUTHLOGINSTUB = "https://www.qa.tax.service.gov.uk/auth-login-stub/gg-sign-in",
-          FEEDBACK_PAGE = "https://www.qa.tax.service.gov.uk/feedback/single-customer-account-frontend",
-          STATEPENSION_PAGE = "https://www.qa.tax.service.gov.uk/check-your-state-pension/account",
-          NI_PAGE = "https://www.qa.tax.service.gov.uk/check-your-state-pension/account/nirecord",
-          PAGE_TIMEOUT_SECS = 60
-        )
-
-      case Environment.Staging =>
-        new Configuration(
-          APP_ROOT =
-            "https://www.staging.tax.service.gov.uk/auth-login-stub/gg-sign-in?continue=%2Fsingle-customer-account",
-          APPROOT = "https://www.staging.tax.service.gov.uk/single-customer-account",
-          AUTHLOGINSTUB = "https://www.staging.tax.service.gov.uk/auth-login-stub/gg-sign-in",
-          FEEDBACK_PAGE = "https://www.staging.tax.service.gov.uk/feedback/single-customer-account-frontend",
-          STATEPENSION_PAGE = "https://www.staging.tax.service.gov.uk/check-your-state-pension/account",
-          NI_PAGE = "https://www.staging.tax.service.gov.uk/check-your-state-pension/account/nirecord",
-          PAGE_TIMEOUT_SECS = 60
-        )
-
-      case _ =>
-        throw new IllegalArgumentException(s"Environment '$environment' not known")
-    }
-}
-
-object Environment extends Enumeration {
-  type Name = Value
-  val Local, dev, Qa, Staging = Value
+  lazy val settings: Configuration = environment.fold(
+    new Configuration(
+      APP_ROOT = "http://localhost:9949/auth-login-stub/gg-sign-in?continue=%2Fsingle-customer-account",
+      APPROOT = "http://localhost:8420/single-customer-account",
+      AUTHLOGINSTUB = "http://localhost:9949/auth-login-stub/gg-sign-in",
+      FEEDBACK_PAGE = "http://localhost:9514/feedback/single-customer-account-frontend",
+      STATEPENSION_PAGE = "http://localhost:9234/check-your-state-pension/account",
+      NI_PAGE = "http://localhost:9234/check-your-state-pension/account/nirecord"
+    )
+  ) { environmentUrl =>
+    new Configuration(
+      APP_ROOT = s"$environmentUrl/auth-login-stub/gg-sign-in?continue=%2Fsingle-customer-account",
+      APPROOT = s"$environmentUrl/single-customer-account",
+      AUTHLOGINSTUB = s"$environmentUrl/auth-login-stub/gg-sign-in",
+      FEEDBACK_PAGE = s"$environmentUrl/feedback/single-customer-account-frontend",
+      STATEPENSION_PAGE = s"h$environmentUrl/check-your-state-pension/account",
+      NI_PAGE = s"$environmentUrl/check-your-state-pension/account/nirecord"
+    )
+  }
 }
