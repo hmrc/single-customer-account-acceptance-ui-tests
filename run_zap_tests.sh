dcc08deea8313995531a2c90006ed57dc4fc2e4c
Rakesh-Eluri <108217580+Rakesh-Eluri@users.noreply.github.com>
2022-11-14 18:20:04 +0000

    Added SCA ZAP tests

--- run_zap_tests.sh
+++ run_zap_tests.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-ENV=${1:-local}
-BROWSER=${2:-chrome}
+set -e
 
-if [ "$BROWSER" = "chrome" ]; then
-    DRIVER="-Dwebdriver.chrome.driver=/opt/homebrew/bin/chromedriver"
-elif [ "$BROWSER" = "firefox" ]; then
-    DRIVER="-Dwebdriver.gecko.driver=/opt/homebrew/bin/geckodriver"
+browser="chrome"
+if [ $# -gt 0  ];
+then
+  browser="$1"
 fi
 
-sbt -Dbrowser=$BROWSER -Denvironment=$ENV $DRIVER "testOnly uk.gov.hmrc.test.ui.cucumber.runner.ZapRunner"
+environment="local"
+
+echo "*** running on $environment using $browser for tags '$tags' ***"
+
+sbt -Dhttp.proxyHost=localhost -Dhttp.proxyPort=11000 -Denvironment="$environment" -Dbrowser="$browser" -Dcucumber.options="--tags '$tags'" clean "testOnly uk.gov.hmrc.test.ui.cucumber.runner.ZapRunner”
