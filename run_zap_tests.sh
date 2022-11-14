ba5404bb4355ebe5b823c7dc37d6e9750725cf79
Rakesh-Eluri <108217580+Rakesh-Eluri@users.noreply.github.com>
2022-11-14 18:32:08 +0000

    Added SCA ZAP tests

--- run_zap_tests.sh
+++ run_zap_tests.sh
@@ -12,4 +12,4 @@ environment="local"
 
 echo "*** running on $environment using $browser for tags '$tags' ***"
 
-sbt -Dhttp.proxyHost=localhost -Dhttp.proxyPort=11000 -Denvironment="$environment" -Dbrowser="$browser" -Dcucumber.options="--tags '$tags'" clean "testOnly uk.gov.hmrc.test.ui.cucumber.runner.ZapRunner”
+sbt -Dhttp.proxyHost=localhost -Dhttp.proxyPort=11000 -Denvironment="$environment" -Dbrowser="$browser" -Dcucumber.options="--tags '$tags'" clean "testOnly uk.gov.hmrc.test.ui.cucumber.runner.ZapRunner"
